ad9a2cfb11e25f0ce0a990c99319ae8d1f2cd359
vymn mohvmd <78211839+vymn@users.noreply.github.com>
2023-02-27 09:10:05 +0200

    init

---
+++ android/app/src/main/kotlin/com/vymn/parker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vymn.parker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/auth/presentation/pages/sign_in_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Login",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(vertical: 120, horizontal: 26),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Email'),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Password'),
+            ),
+            const SizedBox(
+              height: 56,
+            ),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.green, foregroundColor: Colors.white),
+              child: Container(
+                width: 220,
+                height: 26,
+                alignment: Alignment.center,
+                child: const Text(
+                  "SIGN IN",
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
